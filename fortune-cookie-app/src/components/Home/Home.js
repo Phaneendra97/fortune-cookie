053e08fa0b0728a104c62b69cf31707ddc1e3ad9
phaneendra97 <phaneendra1997@gmail.com>
2021-09-05 15:06:40 +0530

    userdetails from ip

--- fortune-cookie-app/src/components/Home/Home.js
+++ fortune-cookie-app/src/components/Home/Home.js
@@ -1,20 +1,61 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./Home.module.css";
 import Flexbox from "flexbox-react";
+import axios from "axios";
 
-const Home = () => (
-  <Flexbox
-    flexDirection="row"
-    alignItems="center"
-    justifyContent="center"
-    minHeight="100vh"
-  >
-    Home Component
-  </Flexbox>
-);
+var userDetails = {
+  country: "",
+  timezone: "",
+  localTime: "",
+  localDate: "",
+};
 
-Home.propTypes = {};
-
-Home.defaultProps = {};
+function Home() {
+  useEffect(() => {
+    getGeoInfo();
+  });
+  return (
+    <Flexbox
+      flexDirection="row"
+      alignItems="start"
+      justifyContent="center"
+      minHeight="100vh"
+      marginTop="20px"
+    >
+      <h2>Fortune Cookie</h2>
+      <Flexbox></Flexbox>
+    </Flexbox>
+  );
+}
 
 export default Home;
+
+const getGeoInfo = () => {
+  axios
+    .get("https://ipapi.co/json/")
+    .then((response) => {
+      let data = response.data;
+      const now = new Date();
+      //now.timezone = data.timezone;
+      let options = {
+          timeZone: data.timezone,
+          year: "numeric",
+          month: "numeric",
+          day: "numeric",
+          hour: "numeric",
+          minute: "numeric",
+          second: "numeric",
+        },
+        formatter = new Intl.DateTimeFormat([], options);
+      console.log(formatter.format(new Date()));
+      console.log("@here@@", new Date(formatter.format(new Date())));
+      userDetails = {
+        country: data.country_name,
+        timezone: data.timezone,
+        localTime: now,
+      };
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
