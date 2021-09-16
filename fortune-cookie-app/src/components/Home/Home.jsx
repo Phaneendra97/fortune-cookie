f1a7ed81a580117af29119bce256b32ca2ce0be7
phaneendra97 <phaneendra1997@gmail.com>
2021-09-17 00:41:04 +0530

    structure improvemenr

--- fortune-cookie-app/src/components/Home/Home.jsx
+++ fortune-cookie-app/src/components/Home/Home.jsx
@@ -8,13 +8,14 @@ export class Home extends Component {
       country: "",
       timezone: "",
       localTime: "",
-      localDate: "",
     },
+    greetings: "",
   };
+
   render() {
     return (
       <Flexbox
-        flexDirection="row"
+        flexDirection="column"
         alignItems="start"
         justifyContent="start"
         minHeight="100vh"
@@ -22,67 +23,71 @@ export class Home extends Component {
         marginLeft="20px"
       >
         <Flexbox>
-          <h2>Fortune Cookie</h2>{" "}
+          <ul class="nav">
+            <li class="nav-item">
+              <a
+                class="nav-link active"
+                style={{ fontSize: "25px" }}
+                aria-current="page"
+              >
+                Fortune Cookie
+              </a>
+            </li>
+          </ul>
         </Flexbox>
-        <Flexbox> </Flexbox>
+        <Flexbox
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="center"
+          height="100%"
+          width="100%"
+        >
+          <h3>{this.state.greetings}</h3>{" "}
+        </Flexbox>
       </Flexbox>
     );
   }
+
+  componentDidMount() {
+    this.getGeoInfo();
+  }
+
+  getGeoInfo = () => {
+    axios
+      .get("https://ipapi.co/json/")
+      .then((response) => {
+        let data = response.data;
+        let options = {
+          timeZone: data.timezone,
+          year: "numeric",
+          month: "numeric",
+          day: "numeric",
+          hour: "numeric",
+          minute: "numeric",
+          second: "numeric",
+        };
+        let greetings = "";
+        const formatter = new Intl.DateTimeFormat([], options);
+        const dateObject = new Date(formatter.format(new Date()));
+        if (dateObject.getHours() < 12) {
+          greetings = "Good Morning!";
+        } else if (dateObject.getHours() > 12 && dateObject.getHours() < 16) {
+          greetings = "Good Afternoon!";
+        } else if (dateObject.getHours() > 16) {
+          greetings = "Good Evening!";
+        }
+        let userDetails = {
+          country: data.country_name,
+          timezone: data.timezone,
+          localTime: dateObject,
+        };
+        this.setState({ userDetails: userDetails, greetings: greetings });
+        console.log("@here", userDetails, greetings);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 }
 
-// function Home() {
-//   useEffect(() => {
-//     getGeoInfo();
-//   });
-//   return (
-//     <Flexbox
-//       flexDirection="row"
-//       alignItems="start"
-//       justifyContent="center"
-//       minHeight="100vh"
-//       marginTop="20px"
-//     >
-//       <h2>Fortune Cookie</h2>
-//       <Flexbox> {userDetails.country}</Flexbox>
-//     </Flexbox>
-//   );
-// }
-
 export default Home;
-
-// const getGeoInfo = () => {
-//   axios
-//     .get("https://ipapi.co/json/")
-//     .then((response) => {
-//       let data = response.data;
-//       let options = {
-//         timeZone: data.timezone,
-//         year: "numeric",
-//         month: "numeric",
-//         day: "numeric",
-//         hour: "numeric",
-//         minute: "numeric",
-//         second: "numeric",
-//       };
-//       let greetings = "";
-//       const formatter = new Intl.DateTimeFormat([], options);
-//       const dateObject = new Date(formatter.format(new Date()));
-//       if (dateObject.getHours() < 12) {
-//         greetings = "Good Morning!";
-//       } else if (dateObject.getHours() > 12 && dateObject.getHours() < 16) {
-//         greetings = "Good Afternoon!";
-//       } else if (dateObject.getHours() > 16) {
-//         greetings = "Good Evening!";
-//       }
-//       userDetails = {
-//         country: data.country_name,
-//         timezone: data.timezone,
-//         localTime: dateObject,
-//         greetings: greetings,
-//       };
-//       console.log("@here", userDetails, greetings);
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//     });
-// };
