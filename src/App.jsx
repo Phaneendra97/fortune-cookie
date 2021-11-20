bccce0ed40b512100a4c9096fa475855e63e388b
phaneendra97 <phaneendra1997@gmail.com>
2021-11-20 18:56:16 +0530

    v1.0 release

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,11 @@ import "./App.css";
 import { Route, Switch, BrowserRouter } from "react-router-dom";
 import Home from "./components/Home/Home";
 import { Component } from "react";
+import { initializeApp } from "firebase/app";
+import firebaseConfig from "./firebaseConfig";
 
 export class App extends Component {
+  app = initializeApp(firebaseConfig);
   render() {
     return (
       <div className="App">
